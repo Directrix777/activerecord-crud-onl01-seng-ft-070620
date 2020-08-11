659b5419298e90bc18ff00e310d67a7299680196
Andrew Ribas <andrew.j.ribas@gmail.com>
2020-08-11 21:43:05 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -64,7 +64,7 @@ end
 def can_find_using_where_clause_and_be_sorted
   # For this test return all movies released after 2002 and ordered by
   # release date descending
-  Movie.connection.execute("SELECT title, release_date, director, lead, in_theaters FROM movies WHERE release_date > 2002 ORDER BY release_date DESC").collect{|attribute_hash| Movie.create(attribute_hash[0], attribute_hash[1], attribute_hash[2], attribute_hash[3], attribute_hash[4])}
+  Movie.connection.execute("SELECT title, release_date, director, lead, in_theaters FROM movies WHERE release_date > 2002 ORDER BY release_date DESC").collect{|attribute_hash| Movie.new_from_db}
 end
 
 def can_be_found_updated_and_saved
